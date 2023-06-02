50eb7f3229030c6a3367509dee6dee4188a6e254
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2023-06-02 12:15:56 +0700

    feat: update-category refs:APT-15

--- src/routers/category.js
+++ src/routers/category.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createCategory, getAll, remove } from "../controllers/category";
+import { createCategory, getAll, remove, updateCategory } from "../controllers/category";
 
 
 
@@ -11,6 +11,6 @@ const router = express.Router();
 router.post("/category", createCategory);
 router.delete("/category/:id", remove)
 router.get("/category", getAll)
-
+router.patch("/category/:id",updateCategory)
 
 export default router;
