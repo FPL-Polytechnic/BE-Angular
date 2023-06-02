371957a2b700e9a8e646c395a6fa85e51f3535d3
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2023-06-02 22:01:12 +0700

    feat: Đăng ký refs:APT-24

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import productRouter from "./routers/product";
 import categoryRouter from "./routers/category";
-
+import authRouter from "./routers/auth"
 const app = express();
 
 app.use(express.json());
@@ -11,7 +11,7 @@ app.use(cors());
 
 app.use("/api", productRouter);
 app.use("/api", categoryRouter);
-
+app.use("/api",authRouter)
 mongoose.connect('mongodb://127.0.0.1:27017/be_angular')
     .then(() => console.log("Kết nối thành công!"));
 
