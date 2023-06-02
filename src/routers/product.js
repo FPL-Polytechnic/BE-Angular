ac21ad0a45363c96969be15f3fd3809df2317baa
Tran Hai Duong <haiduong12122003@gmail.com>
2023-06-02 10:34:07 +0700

    feat : [BE] Xay-dung-list-one-product
    refs : APT-22

--- src/routers/product.js
+++ src/routers/product.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { createProduct, getAll, remove, updateProduct } from "../controllers/product";
+import { createProduct, getAll, getOneProduct, remove, updateProduct } from "../controllers/product";
 
 const router = express.Router();
 
 
 router.post("/product", createProduct);
-router.patch("/product/:id",updateProduct)
-router.get("/product",getAll)
-router.delete("/product/:id",remove)
+router.patch("/product/:id", updateProduct)
+router.get("/product", getAll)
+router.get("/product/:id", getOneProduct)
+router.delete("/product/:id", remove)
 
 
 
