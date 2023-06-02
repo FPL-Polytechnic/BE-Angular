50e938d1ff444412566c77a3456a1847c3a2bd8a
Tran Hai Duong <haiduong12122003@gmail.com>
2023-06-02 12:52:56 +0700

    Updated : 12h50 - 2/6/2023

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -47,14 +47,17 @@ export const remove = async (req, res) => {
 };
 export const getAll = async (req, res) => {
     try {
-        const data = await Category.find().populate("products");
+        const data = await Category.find().populate("products", "-__v -updatedAt -createdAt");
 
         if (data.length == 0) {
             return res.json({
-                message: "Không có sản phẩm nào",
+                message: "Không có danh mục nào",
             });
         }
-        return res.json(data);
+        return res.status(200).json({
+            message: "Lấy tất cả danh mục thành công !",
+            data
+        });
     } catch (error) {
         return res.status(400).json({
             message: error.message
@@ -65,36 +68,38 @@ export const getAll = async (req, res) => {
 export const get = async (req, res) => {
     try {
         const id = req.params.id;
-        const category = await Category.findById(id).populate("products");
-        console.log("category", category);
+        const category = await Category.findById(id).populate("products", "-__v -updatedAt -createdAt");
         if (category.length === 0) {
             return res.status(200).json({
-                message: "Không có sản phẩm",
+                message: "Không có danh mục",
             });
         }
-        return res.status(200).json(category);
+        return res.status(200).json({
+            message: "Lấy 1 danh mục thành công!",
+            category
+        });
     } catch (error) {
         return res.status(400).json({
-            message: error,
+            message: error.message,
         });
     }
 };
 export const updateCategory = async (req, res) => {
     try {
-        const id=req.params.id
-        const categoryUpdate=await Category.findByIdAndUpdate(id,req.body,{
-            new:true
+        const id = req.params.id
+        const categoryUpdate = await Category.findByIdAndUpdate(id, req.body, {
+            new: true
         })
-        if(!categoryUpdate){
+        if (!categoryUpdate) {
             return res.status(400).json({
-                message:"Cập nhập sản phẩm thất bại"
+                message: "Cập nhập danh mục thất bại"
             })
         }
         return res.status(200).json({
-            message:"Cập nhập sản phẩm thành công",
+            message: "Cập nhập danh mục thành công",
             categoryUpdate
         })
-    } 
+    }
     catch (error) {
         res.status(400).json({
             message: error.message
--- src/routers/category.js
+++ src/routers/category.js
@@ -1,12 +1,7 @@
 import express from "express";
 import { createCategory, get, getAll, remove, updateCategory } from "../controllers/category";
 
-
-
-
-
 const router = express.Router();
-
 
 router.post("/category", createCategory);
 router.delete("/category/:id", remove)
