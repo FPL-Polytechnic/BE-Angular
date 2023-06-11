1e8f1bccad28d9f0216a89d506ea01b9a92d430c
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2023-06-11 19:23:55 +0700

    feat:check checkPermission refs:APT-68

--- src/routers/product.js
+++ src/routers/product.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { createProduct, getAll, getOneProduct, remove, updateProduct } from "../controllers/product";
+import checkPermission from "../middlewares/checkPermission";
 
 const router = express.Router();
 
 
-router.post("/product", createProduct);
-router.patch("/product/:id", updateProduct)
+router.post("/product",checkPermission, createProduct);
+router.patch("/product/:id",checkPermission, updateProduct)
 router.get("/product", getAll)
 router.get("/product/:id", getOneProduct)
-router.delete("/product/:id", remove)
+router.delete("/product/:id",checkPermission, remove)
 
 
 
